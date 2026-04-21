4f1b04d2a200b2205b8f476e372773dd1bcc383e
Assma <assma.kebbous@epitech.eu>
2026-04-21 14:35:52 +0200

    ex 1

---
+++ Exam_SQL/ex_01.sql
@@ -0,0 +1,28 @@
+1. 
+SELECT * FROM tablename
+
+2.
+USE dbname
+
+3.
+SELECT * = toute la table
+SELECT colonne = juste la colonne en question de la table
+
+4.
+SELECT DISTINCT colonne FROM tablename
+
+5.
+WHERE
+
+6. 
+WHERE TERMINATED BY 'string'
+WHERE STARTING BY 'string'
+
+7.
+AND c est avec OR c est ou 
+
+8.
+SELECT COUNT(*) FROM tablename
+
+9.
+DELETE c est pour supprimer les ligne de la colonne et DROP supprimer la colonne attention elle doit etre vide 
