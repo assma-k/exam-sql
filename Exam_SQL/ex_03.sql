c94d11a5a847234ded1719267ae6ba27d14ff06e
Assma <assma.kebbous@epitech.eu>
2026-04-21 15:38:35 +0200

    ex 2

--- Exam_SQL/ex_03.sql
+++ Exam_SQL/ex_03.sql
@@ -12,6 +12,7 @@ WHERE id = orders.clients_id
 20.
 SELECT * FROM orders LEFT JOIN clients ON orders.client_id = clients.id
 
-21.
+23.
+SELECT COUNT(*) FROM orders LEFT JOIN clients ON orders.clients_id = clients.id
 
 
