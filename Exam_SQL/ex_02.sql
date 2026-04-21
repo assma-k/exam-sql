25b62db79ea5cd45cc665ba45b12a9e4ea5f401d
Assma <assma.kebbous@epitech.eu>
2026-04-21 14:50:39 +0200

    ex 2

---
+++ Exam_SQL/ex_02.sql
@@ -0,0 +1,32 @@
+10. 
+SELECT * FROM users
+
+11.
+SELECT name FROM users
+
+12.
+SELECT * FROM users
+WHERE age > 25
+
+13.
+SELECT * FROM users
+WHERE city = "Paris"
+
+14.
+SELECT * FROM users
+WHERE name 
+OR
+WHERE firstname 
+STARTING BY "Mar"
+
+15.
+SELECT * FROM users
+WHERE city = "Marseille"
+OR
+WHERE city = "Lille"
+AND age > 18
+AND age < 30
+
+16.
+SELECT * FROM users
+ORDER BY age DESC
