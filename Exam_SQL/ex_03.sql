2b6488ee6e471ccb200883f1a332151544ef67fe
Assma <assma.kebbous@epitech.eu>
2026-04-21 15:25:19 +0200

    ex 2

--- Exam_SQL/ex_03.sql
+++ Exam_SQL/ex_03.sql
@@ -6,3 +6,12 @@ SELECT * FROM orders
 WHERE amount > 100
 
 19.
+SELECT name FROM clients
+WHERE id = orders.clients_id
+
+20.
+SELECT * FROM orders LEFT JOIN clients ON orders.client_id = clients.id
+
+21.
+
+
